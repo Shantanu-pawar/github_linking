c1ff21794722413b90e569ae3c4a49065d369c57
bhai <shantanupawar292@gmail.com>
2023-06-20 10:23:31 +0530

    first commit

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,12 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+
+        int []arr = {1, 45 ,6, 5, 4, 3};
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+
     }
 }
